6829b7a5640ba46738c295daacf6a2d3c706433c
deadpool <iwilljustdiy@gmail.com>
2020-07-06 01:27:52 -0400

    This adds in the basic server funtions. follow along with video and commits for more

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+var http = require("http");
+
+http.createServer(function(request, response)
+    {
+        response.writeHead(200, 
+            {
+                'Content-Type': 'text/plain'
+            });
+            response.end('Hello\n');
+    }).listen(8080);
+
+    console.log('Server is running on port 8080');
